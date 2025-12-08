10231c9b495dfb1203d75c1ca54947c8df3b2264
sebiwi <contact.sebiwi@gmail.com>
2025-12-08 22:59:23 +0100

    refactor(scripts): remove unused scripts

--- scripts/remove-asciinema.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Removing asciinema script tags from blog posts..."
-
-# Remove lines containing asciinema script tags
-find content/blog -name "*.md" -type f -exec sed -i '' '/<script.*asciinema\.org.*<\/script>/d' {} \;
-
-echo "✓ Removed asciinema references"
